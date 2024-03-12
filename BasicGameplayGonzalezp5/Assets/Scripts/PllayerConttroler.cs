8439e9e492d90d2a374d3b02fc19c8812ff293cf
Joel Gonzalez <754537@student.dusd.net>
2024-03-12 13:51:15 -0700

    ya se mueve el vato

---
+++ BasicGameplayGonzalezp5/Assets/Scripts/PllayerConttroler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PllayerConttroler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float horizontalinput;
+    public float speed = 10.0f;
+    public float xRange =10;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(horizontalinput -xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange) 
+        { 
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalinput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalinput * Time.deltaTime * speed);
+    }  
+}
